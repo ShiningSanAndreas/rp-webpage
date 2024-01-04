a96cc2a62a991038af655ac3668f76a253b5a77b
TormiA <tormi.ael@outlook.com>
2024-01-04 16:29:26 +0200

    changed start playing section

--- pages/home.php
+++ pages/home.php
@@ -2,6 +2,8 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
     <link href="../styles/output.css" rel="stylesheet" />
@@ -16,12 +18,12 @@
         <div class="text-white mt-16 mb-6 ">
             <h2 class="text-3xl font-semibold ml-[160px]">Alusta mängimist</h2>
         </div>
-        <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center mb-16">
             <!-- First Image and Text -->
             <div class="relative">
-                <div class="bg-sky-400/30 h-[384px] w-[1120px] absolute top-0 left-0"></div>
-                <img src="../assets/lisakarakter2.jpg" alt="First Image" class="h-[384px] w-[1120px] object-cover">
-                <div class="absolute top-4 left-4 text-white filter">
+                <div class="bg-sky-400/30 h-[352px] w-[1120px] absolute rounded-t-md"></div>
+                <img src="../assets/lisakarakter2.jpg" alt="First Image" class="h-[352px] w-[1120px] object-cover rounded-t-md">
+                <div class="absolute top-4 left-4 text-white">
                     <p class="text-2xl font-bold ml-[32px] mt-[32px]">Logi discordiga sisse..</p>
                     <a href="#">
                         <iconify-icon icon="logos:discord-icon" width="50" height="40" class="ml-10 mt-2"></iconify-icon>
@@ -31,8 +33,8 @@
 
             <!-- Second Image and Text -->
             <div class="relative">
-                <div class="bg-purple-400/30 h-[384px] w-[1120px] absolute top-0 right-0"></div>
-                <img src="../assets/boombox.jpg" alt="Second Image" class="h-[384px] w-[1120px] object-cover ">
+                <div class="bg-purple-400/30 h-[352px] w-[1120px] absolute "></div>
+                <img src="../assets/boombox.jpg" alt="Second Image" class="h-[352px] w-[1120px] object-cover ">
                 <div class="absolute top-4 right-4 text-white">
                     <p class="text-2xl font-bold mr-[32px] mt-[32px]">Täida whitelist..</p>
                     <a href="#">
@@ -43,8 +45,8 @@
 
             <!-- Third Image and Text -->
             <div class="relative">
-                <div class="bg-emerald-300/30 h-[384px] w-[1120px] absolute top-0 left-0"></div>
-                <img src="../assets/eritellimus.jpg" alt="Third Image" class="h-[384px] w-[1120px] object-cover">
+                <div class="bg-emerald-300/30 h-[352px] w-[1120px] absolute rounded-b-md"></div>
+                <img src="../assets/eritellimus.jpg" alt="Third Image" class="h-[352px] w-[1120px] object-cover rounded-b-md">
                 <div class="absolute top-4 left-4 text-white">
                     <p class="text-2xl font-bold ml-[32px] mt-[32px]">Ja hakka mängima!</p>
                     <a href="#">
