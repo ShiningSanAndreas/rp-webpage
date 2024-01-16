678280baaa5bc9fa260a84ee60cc86a5c0953267
TormiA <tormi.ael@outlook.com>
2024-01-16 12:57:09 +0200

    Made vision section skeleton

--- pages/home.php
+++ pages/home.php
@@ -20,7 +20,7 @@
     <!-- Start Playing Section -->
     <div class="container mx-auto text-left">
         <div class="text-white mt-16 mb-6 ">
-            <h2 class="text-3xl font-semibold ml-[160px]">Alusta mängimist</h2>
+            <h2 class="text-3xl font-semibold ml-40">Alusta mängimist</h2>
         </div>
         <div class="flex flex-col items-center mb-16">
             <!-- First Image and Text -->
@@ -28,7 +28,7 @@
                 <img src="../assets/SP_firstpic.png" alt="First Image"
                     class="h-[352px] w-[1120px] object-cover rounded-t-md">
                 <div class="absolute top-4 left-4 text-white">
-                    <p class="text-2xl font-bold ml-[32px] mt-[32px]">Logi discordiga sisse..</p>
+                    <p class="text-2xl font-medium ml-[32px] mt-[32px]">Logi discordiga sisse..</p>
                     <a href="landing.php">
                         <iconify-icon icon="logos:discord-icon" width="50" height="40"
                             class="ml-10 mt-2"></iconify-icon>
@@ -40,7 +40,7 @@
             <div class="relative">
                 <img src="../assets/SP_secpic.png" alt="Second Image" class="h-[352px] w-[1120px] object-cover">
                 <div class="absolute top-4 right-4 text-white">
-                    <p class="text-2xl font-bold mr-[32px] mt-[32px]">Täida whitelist..</p>
+                    <p class="text-2xl font-medium mr-[32px] mt-[32px]">Täida whitelist..</p>
                     <a href="whitelist-form.php">
                         <iconify-icon icon="pajamas:review-list" width="60" height="62" class="ml-24"></iconify-icon>
                     </a>
@@ -52,7 +52,7 @@
                 <img src="../assets/SP_thirdpic.png" alt="Third Image"
                     class="h-[352px] w-[1120px] object-cover rounded-b-md">
                 <div class="absolute top-4 left-4 text-white">
-                    <p class="text-2xl font-bold ml-[32px] mt-[32px]">Ja hakka mängima!</p>
+                    <p class="text-2xl font-medium ml-[32px] mt-[32px]">Ja hakka mängima!</p>
                     <a href="#">
                         <iconify-icon icon="simple-icons:fivem" width="50" height="40"
                             class="ml-10 mt-3"></iconify-icon>
@@ -63,33 +63,39 @@
     </div>
 
     <!-- News Section -->
-    <div class="container-none mx-auto bg-alter text-left">
-        <div class="text-black mt-16 mb-6 ">
-            <h2 class="text-3xl font-semibold ml-[160px] mt-16">Visioon</h2>
-        </div>
-        <div class="flex flex-col items-center ">
-            <!-- First Image and Text -->
-            <div class="flex flex-row mb-16">
-                <img src="../assets/SP_secpic.png" alt="Second Image" class="h-72 w-72">
+    <div class="bg-alter">
+        <div class="container mx-auto">
+            <div class="text-black">
+                <h2 class="text-3xl font-semibold ml-40 pt-16 pb-6">Visioon</h2>
             </div>
+            <div class="flex flex-col px-48">
 
-            <!-- Second Image and Text -->
-            <div class="flex flex-row gap-64 mb-16">
-                <h1>blah blah blah blah</h1>  
-                <img src="../assets/SP_secpic.png" alt="Second Image" class="h-72 w-72">
-                
-            </div>
+                <!-- First Image and Text -->
+                <div class="flex flex-row mb-16 items-center">
+                    <div class="relative">
+                        <h1 class="text-2xl font-medium">Unikaalne taseme süsteem</h1>
+                        <p class="text-xl">lorem ipsum dolor sit amet</p>
+                    </div>
+                    <img src="../assets/mlo.jpg" alt="Second Image" class="h-96 w-96 rounded-md border-4 border-white ml-auto">
+                </div>
 
-            <!-- Third Image and Text -->
-            <div class="relative">
-                <img src="../assets/SP_thirdpic.png" alt="Third Image"
-                    class="h-[352px] w-[1120px] object-cover rounded-b-md">
-                <div class="absolute top-4 left-4 text-white">
-                    <p class="text-2xl font-bold ml-[32px] mt-[32px]">Ja hakka mängima!</p>
-                    <a href="#">
-                        <iconify-icon icon="simple-icons:fivem" width="50" height="40"
-                            class="ml-10 mt-3"></iconify-icon>
-                    </a>
+                <!-- Second Image and Text -->
+                <div class="flex flex-row mb-16 items-center">
+                    <img src="../assets/mlo.jpg" alt="Second Image" class="h-96 w-96 rounded-md border-4 border-white">
+                    <div class="text-right relative ml-auto">
+                        <h1 class="text-2xl font-medium">Tühi tiitel</h1>
+                        <p class="text-xl">lorem ipsum dolor sit amet</p>
+                    </div>
+                </div>
+
+                <!-- Third Image and Text -->
+                <div class="flex flex-row mb-16 items-center">
+                    <div class="relative">
+                        <h1 class="text-2xl font-medium">Kuritegevuse impeerium</h1>
+                        <p class="text-xl">Alusta enda jõuk ning saa kõige rikkamaks ja mõjukamaks krimi
+                            bossiks</p>
+                    </div>
+                    <img src="../assets/mlo.jpg" alt="Third Image" class="h-96 w-96 rounded-md border-4 border-white ml-auto">
                 </div>
             </div>
         </div>
