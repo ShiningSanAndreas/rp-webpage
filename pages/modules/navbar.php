a16bbfd4e4456da1097c6185822489648e1ab7f7
TormiA <142418637+TormiA@users.noreply.github.com>
2024-01-19 16:00:57 +0200

    login logic for navbar
    started making login logic for navbar

--- pages/modules/navbar.php
+++ pages/modules/navbar.php
@@ -1,4 +1,41 @@
 <html>
+<?php
+include("../config.php");
+
+try {
+  $db = new PDO($configDsn, $configDbName, $configDbPw);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+}
+
+session_start();
+
+$isLoggedIn = isset($_SESSION["logged_in"]) && $_SESSION["logged_in"];
+
+// Check if the current page is not the landing page or homepage
+$currentPage = basename($_SERVER["PHP_SELF"]);
+if (!$isLoggedIn && $currentPage !== 'landing.php' && $currentPage !== 'home.php') {
+  header("Location: landing.php");
+  exit();
+}
+
+// Extract user data only if logged in
+$userData = isset($_SESSION["userData"]) ? $_SESSION["userData"] : [];
+extract($userData);
+
+$avatar_url = $isLoggedIn ? "https://cdn.discordapp.com/avatars/$discord_id/$avatar.jpg" : "";
+$current_page = "home";
+
+// Check if the database connection is successful
+if ($db) {
+  $players_money = getAllPlayersMoney($db);
+  $_SESSION["userBalance"] = $isLoggedIn ? getUserBalance($discord_id, $db) : 0;
+  $_SESSION["whitelist_status"] = $isLoggedIn ? isUserWhitelisted($discord_id, $db) : false;
+  $players_total = getAllPlayers($db);
+  $characterData = $isLoggedIn ? getUserCharacters($discord_id, $db) : [];
+}
+?>
 
 <head>
   <link href="../styles/output.css" rel="stylesheet" />
@@ -27,33 +64,49 @@
         <span class="self-center text-2xl font-semibold whitespace-nowrap text-tekst">ShiningRP</span>
       </a>
       <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        <button type="button" class="flex text-sm bg-light rounded-full md:me-0 focus:ring-4 focus:ring-light"
-          id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
-          data-dropdown-placement="right">
-          <img class="w-12 h-12 rounded-full" src="../assets/boombox.jpg" alt="user photo">
-        </button>
-        <div class="px-4">
-          <!-- You can fetch user information dynamically using PHP -->
-          <span class="block text-md font-medium text-tekst">Tormi</span>
-          <div class="flex flex-row justify-center">
-            <span class="block text-md font-medium text-tekst ">200</span>
-            <img class="w-4 h-4 rounded-full ml-1 mt-1" src="../assets/SSACoinTop.png" alt="balance">
+        <?php if ($isLoggedIn) { ?>
+          <button type="button" class="flex text-sm bg-light rounded-full md:me-0 focus:ring-4 focus:ring-light"
+            id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
+            data-dropdown-placement="right">
+            <img class="w-12 h-12 rounded-full" src="../assets/boombox.jpg" alt="user photo">
+          </button>
+          <div class="px-4">
+            <!-- You can fetch user information dynamically using PHP -->
+            <span class="block text-md font-medium text-tekst">
+              <?php echo $username; ?>
+            </span>
+            <div class="flex flex-row justify-center">
+              <span class="block text-md font-medium text-tekst ">
+                <?php echo $_SESSION["userBalance"]; ?>
+              </span>
+              <img class="w-4 h-4 rounded-full ml-1 mt-1" src="../assets/SSACoinTop.png" alt="balance">
+            </div>
           </div>
-        </div>
-        <!-- Dropdown menu -->
-        <div class="z-50 hidden text-base list-none bg-primary rounded-lg" id="user-dropdown">
-          <ul class="py-2" aria-labelledby="user-menu-button">
-            <li>
-              <a href="#" class="block px-4 py-2 text-sm text-tekst hover:bg-accent">Sign out</a>
-            </li>
-          </ul>
-        </div>
-        <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-tekst rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-user" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
+          <!-- Dropdown menu -->
+          <div class="z-50 hidden text-base list-none bg-primary rounded-lg" id="user-dropdown">
+            <ul class="py-2" aria-labelledby="user-menu-button">
+              <li>
+                <a href="#" class="block px-4 py-2 text-sm text-tekst hover:bg-accent">Sign out</a>
+              </li>
+            </ul>
+          </div>
+        <?php } else { ?>
+          <a href="landing.php">
+            <button type="button"
+              class="text-tekst bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-tekst text-xl px-5 py-2.5 text-center">
+              Logi Sisse
+            </button>
+          </a>
+        <?php } ?>
+        <button data-collapse-toggle="navbar-user" type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-tekst rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200"
+          aria-controls="navbar-user" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15" />
+          </svg>
+        </button>
       </div>
       <div class="items-center hidden w-full md:flex md:w-auto md:order-1 justify-center" id="navbar-user">
         <ul
