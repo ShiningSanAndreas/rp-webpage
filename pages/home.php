ea7f639289e88debf310ea1da7bf15ddc350a7ea
TormiA <142418637+TormiA@users.noreply.github.com>
2024-01-25 11:29:30 +0200

    vision section into 2 sections

--- pages/home.php
+++ pages/home.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include("../config.php");
 
 try {
@@ -29,54 +29,8 @@ $current_page = "home";
 
     <?php include("./modules/caroussel.php") ?>
 
-    <!-- Start Playing Section -->
-    <div class="container mx-auto text-left">
-        <div class="text-tekst mt-16 mb-10 flex justify-center">
-            <h2 class="text-3xl font-semibold">Alusta mängimist</h2>
-        </div>
-        <div class="flex flex-col items-center mb-16">
-            <!-- First Image and Text -->
-            <div class="relative">
-                <img src="../assets/SP_firstpic.png" alt="First Image"
-                    class="h-[352px] w-[1120px] object-cover rounded-t-md">
-                <div class="absolute top-4 left-4 text-tekst">
-                    <p class="text-2xl font-medium ml-[32px] mt-[32px]">Logi discordiga sisse..</p>
-                    <a href="landing.php">
-                        <iconify-icon icon="logos:discord-icon" width="50" height="40"
-                            class="ml-10 mt-2"></iconify-icon>
-                    </a>
-                </div>
-            </div>
-            <!-- Second Image and Text -->
-            <div class="relative">
-                <img src="../assets/SP_secpic.png" alt="Second Image" class="h-[352px] w-[1120px] object-cover">
-                <div class="absolute top-4 right-4 text-tekst">
-                    <p class="text-2xl font-medium mr-[32px] mt-[32px]">Täida whitelist..</p>
-                    <a href="whitelist-form.php">
-                        <iconify-icon icon="pajamas:review-list" width="60" height="62" class="ml-24"></iconify-icon>
-                    </a>
-                </div>
-            </div>
-            <img src="../assets/arrowOne.svg" alt="" class="absolute mt-24" />
-            <!-- Third Image and Text -->
-            <div class="relative">
-                <img src="../assets/SP_thirdpic.png" alt="Third Image"
-                    class="h-[352px] w-[1120px] object-cover rounded-b-md">
-                <div class="absolute top-4 left-4 text-tekst">
-                    <p class="text-2xl font-medium ml-[32px] mt-[32px]">Ja hakka mängima!</p>
-                    <a href="#">
-                        <iconify-icon icon="simple-icons:fivem" width="50" height="40"
-                            class="ml-10 mt-3"></iconify-icon>
-                    </a>
-                </div>
-            </div>
-            <img src="../assets/arrowTwo.svg" alt="" class="absolute mt-[450px]" />
-        </div>
-    </div>
-
-    <!-- News Section -->
     <div class="bg-primary">
-        <div class="container mx-auto">
+        <div class="container mx-auto ">
             <div class="text-tekst flex justify-center">
                 <h2 class="text-3xl font-semibold pt-16 pb-6">Visioon</h2>
             </div>
@@ -107,7 +61,65 @@ $current_page = "home";
                         </p>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
 
+    <!-- Start Playing Section -->
+    <div class="container mx-auto text-left">
+        <div class="text-tekst mt-16 mb-10 flex justify-center">
+            <h2 class="text-3xl font-semibold">Alusta mängimist</h2>
+        </div>
+        <div class="flex flex-col items-center mb-16">
+
+            <!-- First Image and Text -->
+            <div class="relative">
+                <img src="../assets/SP_firstpic.png" alt="First Image"
+                    class="h-[352px] w-[1120px] object-cover rounded-t-md">
+                <div class="absolute top-4 left-4 text-tekst">
+                    <p class="text-2xl font-medium ml-[32px] mt-[32px]">Logi discordiga sisse..</p>
+                    <a href="landing.php">
+                        <iconify-icon icon="logos:discord-icon" width="50" height="40"
+                            class="ml-10 mt-2"></iconify-icon>
+                    </a>
+                </div>
+            </div>
+
+            <!-- Second Image and Text -->
+            <div class="relative">
+                <img src="../assets/SP_secpic.png" alt="Second Image" class="h-[352px] w-[1120px] object-cover">
+                <div class="absolute top-4 right-4 text-tekst">
+                    <p class="text-2xl font-medium mr-[32px] mt-[32px]">Täida whitelist..</p>
+                    <a href="whitelist-form.php">
+                        <iconify-icon icon="pajamas:review-list" width="60" height="62" class="ml-24"></iconify-icon>
+                    </a>
+                </div>
+            </div>
+            <img src="../assets/arrowOne.svg" alt="" class="absolute mt-24" />
+
+            <!-- Third Image and Text -->
+            <div class="relative">
+                <img src="../assets/SP_thirdpic.png" alt="Third Image"
+                    class="h-[352px] w-[1120px] object-cover rounded-b-md">
+                <div class="absolute top-4 left-4 text-tekst">
+                    <p class="text-2xl font-medium ml-[32px] mt-[32px]">Ja hakka mängima!</p>
+                    <a href="#">
+                        <iconify-icon icon="simple-icons:fivem" width="50" height="40"
+                            class="ml-10 mt-3"></iconify-icon>
+                    </a>
+                </div>
+            </div>
+            <img src="../assets/arrowTwo.svg" alt="" class="absolute mt-[450px]" />
+        </div>
+    </div>
+
+    <!-- News Section -->
+    <div class="bg-primary">
+        <div class="container mx-auto">
+            <div class="text-tekst flex justify-center">
+                <h2 class="text-3xl font-semibold pt-16 pb-6">Visioon</h2>
+            </div>
+            <div class="flex flex-col px-48">
                 <!-- Third Image and Text -->
                 <div class="flex flex-row mb-16 items-center">
                     <div class="relative text-tekst p-4 w-1/2">
@@ -133,8 +145,9 @@ $current_page = "home";
                     </div>
                 </div>
                 <div class="flex items-center justify-center">
-                <h1 class="text-2xl text-tekst mb-16">Liitu meiega, kus igal sammul võib avaneda uus ja põnev maailm. Ole
-                    osa millestki suuremast!</h1>
+                    <h1 class="text-2xl text-tekst mb-16">Liitu meiega, kus igal sammul võib avaneda uus ja põnev
+                        maailm. Ole
+                        osa millestki suuremast!</h1>
                 </div>
             </div>
         </div>
